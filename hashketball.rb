3511086f257cd0a7203f7f59633899e3572a5563
Romario Gustave <romagustave10@gmail.com>
2019-10-18 08:57:19 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -65,20 +65,20 @@ def game_hash
 
 away => { 
     :team_name => "Charlotte hornets"
-    :colors => ["Black", "white"]
+    :colors => ["Turquoise", "Purple"]
     {
     :players [
   {
-    :player_name => "Alan Anderson",
-    :number => 0 ,
-    :shoe => 16,
-    :points => 22,
-    :rebounds => 12,
-    :assists => 12,
-    :steals => 3,
-    :blocks => 1 ,
-    :slam_dunks => 1 ,
-
+    :player_name => "",
+    :number =>  ,
+    :shoe => ,
+    :points => ,
+    :rebounds => ,
+    :assists => ,
+    :steals => ,
+    :blocks => ,
+    :slam_dunks =>,
+}
 
 
 
