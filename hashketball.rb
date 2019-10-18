20fdb733086cd74494a33f8831171aa55076e455
Romario Gustave <romagustave10@gmail.com>
2019-10-18 08:48:47 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -7,31 +7,30 @@ def game_hash
     {
     :players [
   {
-    :player_name => "Alan Anderson"
-    :number => 0 
-    :shoe => 16
-    :points => 22
-    :rebounds => 12
-    :assists => 12
-    :steals => 3
-    :blocks => 1 
-    :slam_dunks => 1 
+    :player_name => "Alan Anderson",
+    :number => 0 ,
+    :shoe => 16,
+    :points => 22,
+    :rebounds => 12,
+    :assists => 12,
+    :steals => 3,
+    :blocks => 1 ,
+    :slam_dunks => 1 ,
+  }
+  {
+    :player_name => "Reggie Evans",
+    :number => 30,
+    :shoe1 => 14,
+    :points => 12,
+    :rebounds =>12,
+    :assists => 12,
+    :steals => 12,
+    :blocks => 12,
+    :slam_dunks => 7
   }
   {
     :player_name => 
-    :number
-    :shoe
-    :points
-    :rebounds
-    :assists
-    :steals
-    :blocks
-    :slam_dunks
-  }
-  {
-    :player_name => 
-    :number
-    :number
+    :number:
     :shoe
     :points
     :rebounds
